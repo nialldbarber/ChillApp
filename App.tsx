8930753c7e788294e28dd0121b2fc4589ee1c1b5
Niall <nialldbarber@gmail.com>
2021-10-06 20:49:01 +0100

    first commit

---
+++ App.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
+
+export default function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Text>I hope this works!</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
