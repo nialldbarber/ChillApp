6552c44a9a0290efe63e5e164a823a9e2f5ddb2b
Niall <nialldbarber@gmail.com>
2021-10-06 22:49:47 +0100

    Got a few libs working

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
-import * as React from 'react';
+import React from 'react';
+import {useColorScheme} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
+import {Provider} from 'react-redux';
+import {store} from './src/store';
 import RootNavigator from './src/components/Navigators/RootNavigator';
+import getTheme from './src/styles/theme';
 
 export default function App() {
+  const scheme = useColorScheme();
+
   return (
-    <NavigationContainer>
-      <RootNavigator />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer theme={getTheme(scheme)}>
+        <RootNavigator />
+      </NavigationContainer>
+    </Provider>
   );
 }
